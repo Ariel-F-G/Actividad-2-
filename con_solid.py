1c4138b9263ee44d97a5a9c172e9194b431feac6
Ariel F.G <60789996+ariel0106fg@users.noreply.github.com>
2020-09-30 06:36:01 -0500

    Create con_solid.py

---
+++ con_solid.py
@@ -0,0 +1,16 @@
+import time
+# Ariel Fernández Gualdrón
+class figuras_geometricas:
+    print("Bienvenido , el dia de hoy calcularemos el area de un triangulo y un rectangulo")
+    time.sleep (2)
+    print("Ingrese los datos en el orden que se solicitan")
+    time.sleep (2)
+    print("iniciaremos con el area del triangulo")
+    time.sleep (2)
+    from triangulo import triangulo 
+    time.sleep (2)
+    print("ahora calcularemos el area del rectangulo")
+    time.sleep (2)
+    from rectangulo import rectangulo
+    time.sleep (2)
+    print("gracias")
