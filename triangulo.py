05056af9822c37265b506f9a584c70e3271f6180
Ariel F.G <60789996+ariel0106fg@users.noreply.github.com>
2020-09-30 06:36:54 -0500

    Create triangulo.py

---
+++ triangulo.py
@@ -0,0 +1,7 @@
+class triangulo:
+    print("Ingrese la base en centimetros")
+    base1= int(input())
+    print("Ahora ingrese la altura en centimetros")
+    altura= int(input())
+    print("El area del triangulo es: ")
+    print(((base1*altura)/2),"centimetros")
